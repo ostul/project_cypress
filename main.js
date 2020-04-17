c63b7f354818f63c07117482c0ee16ca4c49d5f1
ostul <ostul@softserveinc.com>
2020-04-17 12:58:53 +0300

    init

--- main.js
+++ main.js
@@ -1,8 +1,2 @@
-import {TodoPage} from "./cypress/page_objects/todo-page"
-
-exports.TodoPage = TodoPage
-
-
-// Not Remove Version 1.0
-// exports.TodoPage = require("./cypress/page_objects/todo-page").TodoPage
-// exports.HomePage = require("./cypress/page_objects/home-page").HomePage
+exports.TodoPage = require("./cypress/page_objects/todo-page").TodoPage
+exports.HomePage = require("./cypress/page_objects/home-page").HomePage
