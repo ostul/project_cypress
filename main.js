03f005cad4b8ddc651fa1c31b664e09db4c31416
ostul <ostul@softserveinc.com>
2020-04-17 13:26:13 +0300

    init

--- main.js
+++ main.js
@@ -1,2 +1,4 @@
-exports.TodoPage = require("./cypress/page_objects/todo-page").TodoPage
+const todoPage = require("./cypress/page_objects/todo-page")
+exports.TodoPage = todoPage.TodoPage
+
 exports.HomePage = require("./cypress/page_objects/home-page").HomePage
